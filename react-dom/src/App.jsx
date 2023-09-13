62378f9819bcee33691600a259203e54858d9c6e
Anderson <andres.salazar.p@uni.pe>
2023-09-13 10:00:52 -0500

    Trabajo iniciando react-dom

--- react-dom/src/App.jsx
+++ react-dom/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <>
-      <h2>Create React DOM</h2>
+      <h2 className=' text-center font-extrabold text-indigo-700 p-7 max-w-7xl bg-red-500'>Create React DOM</h2>
     </>
   )
 }
